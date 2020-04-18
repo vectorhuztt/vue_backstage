221b423bc40f19de0e89d0554ea3b744d865fdb4
vector <vectorztt@163.com>
2020-04-18 21:29:51 +0800

    还原api_urls文件

--- src/network/api_url.js
+++ src/network/api_url.js
@@ -1,15 +1,66 @@
-import { request } from './request'; 
-import qs from 'qs'
+import { request } from './request';
+import qs from 'qs';
+// 登录
 export function getLoginData(params) {
     return request({
         method: 'post',
         url: 'login/',
-        data: qs.stringify(params)
+        data: qs.stringify(params),
     });
 }
-export function getLabelData() {
+
+// 获取标签列表
+export function getLabelData(params) {
     return request({
         method: 'get',
-        url: 'label/'
-    })
+        url: 'label/',
+        params,
+    });
+}
+
+export function getUsersData(params) {
+    return request({
+        method: 'get',
+        url: 'users/',
+        params,
+    });
+}
+
+export function getUserTypes() {
+    return request({
+        method: 'get',
+        url: 'types/',
+    });
+}
+
+export function addUserData(params) {
+    return request({
+        method: 'post',
+        url: 'register/',
+        data: qs.stringify(params),
+    });
+}
+
+export function getUserInfo(params) {
+    return request({
+        method: 'get',
+        url: `user-action/`,
+        params,
+    });
+}
+
+export function UpdateUserInfo(params) {
+    return request({
+        method: 'put',
+        url: 'user-action/',
+        params,
+    });
+}
+
+export function DeleteUser(params) {
+    return request({
+        method: 'delete',
+        url: 'user-action/',
+        params,
+    });
 }
